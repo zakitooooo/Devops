c55a019f9ffd20bd2000b29222f35d8f4be7f1db
zakariastrong <114486087+zakariastrong@users.noreply.github.com>
2026-01-19 20:03:11 +0100

    feat: use DATABASE_URL for Railway

--- app/database.py
+++ app/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 import os
-db_host = os.getenv("DB_HOST", "localhost")
-data_url = f"postgresql://devops:devops123@{db_host}:5432/devops_db"
-engine = create_engine(data_url)
+database_url = os.getenv("DATABASE_URL")
+
+if database_url is None:
+    db_host = os.getenv("DB_HOST", "localhost")
+    database_url = f"postgresql://devops:devops123@{db_host}:5432/devops_db"
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(bind=engine)
 
